84aa8bb15f4dc78270b2696d9df1da0e8a2823dc
Jan Sarenik <jajomojo@gmail.com>
2021-06-30 15:12:04 +0200

    Current UX: click to re-compute
    Also changed input to type number - is nice on mobile.

--- bth.js
+++ bth.js
@@ -1,72 +1,81 @@
 var e, d, s, y = 0;
 
 function selectText() {
-  const input = document.getElementById('log');
-  input.focus();
-  input.select();
+  $('#log').select();
 }
+function selectNum() {
+  $('#number').select();
+}
+
 function readyFn( jQuery ) {
   $("#number").focus();
+  log("Hello!");
+  log("");
+  log("This is Bit Trade Helper.");
+  log("Focus this textarea to");
+  log("recompute with current");
+  log("values above.");
 }
 
-$( window ).on( "load", readyFn );
-$( document ).ready( readyFn );
+$(window).on("load", readyFn);
 
     // simple logging function
-    function log() {
-      var args = Array.prototype.slice.call(arguments);
-      console.log.apply(null, args);
-      args = args.map(function(a) { if (typeof(a) == "string") return a; else return JSON.stringify(a); });
-      document.getElementById("log").textContent += args.join(" ") + "\n";
-    }
+function log() {
+  var args = Array.prototype.slice.call(arguments);
+    console.log.apply(null, args);
+    args = args.map(function(a) { if (typeof(a) == "string") return a; else return JSON.stringify(a); });
+  document.getElementById("log").textContent += args.join(" ") + "\n";
+}
 
 function genoutput() {
-var time = (new Date()).getTime();
-document.getElementById("log").textContent = "";
-e=d=s=y=0;
-mywhat = $('#what').val();
-mynumber = $('#number').val().replace(new RegExp("[^0-9]", "g"), "");
+  var time = (new Date()).getTime();
+  document.getElementById("log").textContent = "";
+  e=d=s=y=0;
+  mywhat = $('#what').val();
+  mynumber = $('#number').val().replace(new RegExp("[^0-9]", "g"), "");
 
-    $.get("https://api.coindesk.com/v1/bpi/currentprice.json", function(data){
-      log("```")
-      log("  Bit Trade Helper");
-      log(" bth.patocka.eu.org");
-      log("====================");
-      log("Timestamp:", Math.floor(time/1000));
-      log("  USD: " + data.bpi.USD.rate);
-      log("  EUR: " + data.bpi.EUR.rate);
-switch(mywhat) {
-  case "EUR":
-    y = data.bpi.EUR.rate;
-    y = y.replace(',','');
-    log("And " + mynumber + " EUR is ...");
-    result = (1 / y * mynumber).toFixed(8)
-    log("  " + result + " BTC");
-    log("  " + (result * 100000000).toFixed(0) + " sat");
-    break;
-  case "USD":
-    y = data.bpi.USD.rate;
-    y = y.replace(',','');
-    log("And " + mynumber + " USD is ...");
-    result = (1 / y * mynumber).toFixed(8)
-    log("  " + result + " BTC");
-    log("  " + result * 100000000 + " sat");
-    break;
-  case "satoshi":
-    y = data.bpi.USD.rate;
-    y = y.replace(',','');
-    log(mynumber + " sat is ...");
-    resultd = (data.bpi.USD.rate.replace(',','')/100000000*mynumber).toFixed(2)
-    resulte = (data.bpi.EUR.rate.replace(',','')/100000000*mynumber).toFixed(2)
-    log("  USD: " + resultd);
-    log("  EUR: " + resulte);
-    break;
-}
-      $.get("https://mempool.space/api/v1/fees/recommended", function(data){
-        fee = data.hourFee;
-        log("Recommended fee is");
-        log("  " + fee + " sat/vB");
-        log("```");
-      }, "json");
+  $.get("https://api.coindesk.com/v1/bpi/currentprice.json", function(data){
+    log("```")
+    log("  Bit Trade Helper");
+    log(" bth.patocka.eu.org");
+    log("====================");
+    log("Timestamp:", Math.floor(time/1000));
+    log("  USD: " + data.bpi.USD.rate);
+    log("  EUR: " + data.bpi.EUR.rate);
+    if(mynumber != 0) {
+    switch(mywhat) {
+      case "EUR":
+        y = data.bpi.EUR.rate;
+        y = y.replace(',','');
+        log("And " + mynumber + " EUR is ...");
+        result = (1 / y * mynumber).toFixed(8)
+        log("  " + result + " BTC");
+        log("  " + (result * 100000000).toFixed(0) + " sat");
+        break;
+      case "USD":
+        y = data.bpi.USD.rate;
+        y = y.replace(',','');
+        log("And " + mynumber + " USD is ...");
+        result = (1 / y * mynumber).toFixed(8)
+        log("  " + result + " BTC");
+        log("  " + result * 100000000 + " sat");
+        break;
+      case "satoshi":
+        y = data.bpi.USD.rate;
+        y = y.replace(',','');
+        log(mynumber + " sat is ...");
+        resultd = (data.bpi.USD.rate.replace(',','')/100000000*mynumber).toFixed(2)
+        resulte = (data.bpi.EUR.rate.replace(',','')/100000000*mynumber).toFixed(2)
+        log("  USD: " + resultd);
+        log("  EUR: " + resulte);
+        break;
+    }
+    } // if mynumber != 0
+    $.get("https://mempool.space/api/v1/fees/recommended", function(data){
+      fee = data.hourFee;
+      log("Recommended fee is");
+      log("  " + fee + " sat/vB");
+      log("```");
     }, "json");
+  }, "json");
 }
